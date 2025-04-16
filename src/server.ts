d2ef6cbeff8abf417382e60149acb2c776689268
kartikey <kartikkey0414@gmail.com>
2025-04-16 15:19:03 +0530

    Added basic route and controller

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+import { pingRouter } from './routers/ping.router';
+import { serverConfig } from './config';
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/ping', (req, res) => {
-    res.send('pong')
-});
+app.use(pingRouter);
 
-app.listen(PORT, () => {
+app.listen(serverConfig.PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
